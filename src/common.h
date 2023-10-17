c3d283b45ad63cfef7b24edf50e309b21bc4053b
painnick <painnick@gmail.com>
2023-10-17 21:48:00 +0900

    FIX. MP3 TX Pin

--- src/common.h
+++ src/common.h
@@ -4,10 +4,7 @@
 // Recommended pins include 2,4,12-19,21-23,25-27,32-33
 // for the ESP32-S2 the GPIO pins are 1-21,26,33-42
 
-#define PIN_MP3_TX 23 // dfplayer(FIXED)
-#define PIN_MP3_RX 32 // dfplayer(FIXED)
-
-#define PIN_MP3_TX 23 // dfplayer(FIXED)
+#define PIN_MP3_TX 33 // dfplayer(FIXED)
 #define PIN_MP3_RX 32 // dfplayer(FIXED)
 #define PIN_MP3_BUSY 35
 
