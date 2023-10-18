54e768d98468e8929b648be911fac141cc23f448
painnick <painnick@gmail.com>
2023-10-19 01:49:31 +0900

    REFACTORING.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <esp_log.h>
 #include <ESP32Servo.h>
+#include <ESP32PWM.h>
 
 #include "common.h"
 #include "Mp3Controller.h"
@@ -8,20 +9,46 @@
 #define MAIN_TAG "Main"
 
 Servo turretServo;
-Servo radarServo;
-Servo gatlingServo;
+
+
+#define LIGHT_BASE 32
+#define LIGHT_STEP 24
+#define LIGHT_MAX (LIGHT_BASE + LIGHT_STEP * 3)
 
 [[noreturn]] void taskSensor(__attribute__((unused)) void *params) {
     while (true) {
-        delay(10);
+        ESP_LOGI(MAIN_TAG, "Sensor");
+
+        ledcWrite(CH_SENSOR_LIGHT, LIGHT_BASE);
+        delay(150);
+        ledcWrite(CH_SENSOR_LIGHT, LIGHT_BASE + LIGHT_STEP);
+        delay(150);
+        ledcWrite(CH_SENSOR_LIGHT, LIGHT_MAX);
+        delay(300);
+        ledcWrite(CH_SENSOR_LIGHT, LIGHT_BASE + LIGHT_STEP);
+        delay(150);
+        ledcWrite(CH_SENSOR_LIGHT, LIGHT_BASE);
+        delay(150);
+        ledcWrite(CH_SENSOR_LIGHT, 0);
+        delay(1000 * random(5, 10));
+    }
+}
+
+[[noreturn]] void taskRadar(__attribute__((unused)) void *params) {
+    while (true) {
+        ESP_LOGI(MAIN_TAG, "Radar");
+
+        ledcWrite(CH_RADAR_MOTOR, 47);
+        delay(1000 * 3);
+        ledcWrite(CH_RADAR_MOTOR, 0);
+        delay(1000 * 10);
     }
 }
 
 [[noreturn]] void taskTurret(__attribute__((unused)) void *params) {
     while (true) {
-#ifdef DEBUG
         ESP_LOGI(MAIN_TAG, "Turret move");
-#endif
+
         turretServo.write(random() % 30);
         delay(1000 * 10);
     }
@@ -30,20 +57,25 @@ Servo gatlingServo;
 bool isGatlingOn = false;
 
 void taskGatling(__attribute__((unused)) void *params) {
+    ESP_LOGI(MAIN_TAG, "Fire!");
+
     isGatlingOn = true;
+
     dfmp3.playAdvertisement(1);
     dfmp3.loop();
 
-    gatlingServo.write(30);
+    delay(300);
 
-    for (int i = 0; i < 5; i++) {
+    ledcWrite(CH_GATLING_MOTOR, 255);
+
+    for (int i = 0; i < 20; i++) {
         digitalWrite(PIN_GATLING_LIGHT, HIGH);
-        delay(100);
+        delay(30);
         digitalWrite(PIN_GATLING_LIGHT, LOW);
-        delay(100);
+        delay(30);
     }
 
-    gatlingServo.write(0);
+    ledcWrite(CH_GATLING_MOTOR, 0);
 
     dfmp3.stopAdvertisement();
     isGatlingOn = false;
@@ -54,32 +86,32 @@ void setup() {
 #ifdef DEBUG
     ESP_LOGI(MAIN_TAG, "Setup...");
 #endif
+    // Gatling
+    pinMode(PIN_GATLING_BUTTON, INPUT);
     pinMode(PIN_GATLING_LIGHT, OUTPUT);
 
+    ledcSetup(CH_GATLING_MOTOR, 1000, 8);
+    ledcAttachPin(PIN_GATLING_MOTOR, CH_GATLING_MOTOR);
+
+    // Sensor Light
     ledcSetup(CH_SENSOR_LIGHT, 1000, 8);
     ledcAttachPin(PIN_SENSOR_LIGHT, CH_SENSOR_LIGHT);
-
-    pinMode(PIN_GATLING_BUTTON, INPUT);
-
-    setupSound();
-    delay(1000);
-
-    playBackground();
-
-    radarServo.attach(PIN_RADAR_SERVO);
-
-    turretServo.attach(PIN_TURRET_SERVO);
-    turretServo.write(0);
-
-    gatlingServo.attach(PIN_GATLING_SERVO);
 
     xTaskCreate(
             taskSensor,
             "Sensor",
             10000,
             nullptr,
-            1,
+            2,
             nullptr);
+
+    // Search Light
+    pinMode(PIN_SEARCH_BUTTON, INPUT);
+    pinMode(PIN_SEARCH_LIGHT, OUTPUT);
+
+    // Turret
+    turretServo.attach(PIN_TURRET_SERVO, 500, 2500);
+    turretServo.write(0);
 
     xTaskCreate(
             taskTurret,
@@ -88,6 +120,26 @@ void setup() {
             nullptr,
             1,
             nullptr);
+
+    // Radar
+    ledcSetup(CH_RADAR_MOTOR, 1000, 8);
+    ledcAttachPin(PIN_RADAR_MOTOR, CH_RADAR_MOTOR);
+
+    xTaskCreate(
+            taskRadar,
+            "Radar",
+            10000,
+            nullptr,
+            3,
+            nullptr);
+
+    // Sound
+    pinMode(PIN_MP3_BUSY, INPUT);
+
+    setupSound();
+    delay(1000);
+
+    playBackground();
 }
 
 int lastGatlingButtonState = HIGH;
